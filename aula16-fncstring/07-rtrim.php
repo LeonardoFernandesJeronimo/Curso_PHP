3aeb10fedcadfdbf0d3c6d3855a278eba1a003cb
Leonardo Fernandes Jeronimo <leonardo.lfj133@gmail.com>
2022-09-21 18:00:02 -0300

    rtrim

---
+++ aula16-fncstring/07-rtrim.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+<div>
+    <?php 
+        $nome = "   Jose da Silva   ";
+        echo (strlen($nome));
+        
+        $novo = rtrim($nome);
+        echo("<br>$novo ");
+
+        echo(strlen($novo));
+    ?>
+</div>
+</body>
+</html>
