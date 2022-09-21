f0eb5fd6e9264a7bac0f36917d3da1de23c0cba8
Leonardo Fernandes Jeronimo <leonardo.lfj133@gmail.com>
2022-09-21 18:20:39 -0300

    chr

---
+++ aula16-fncstring/12-chr.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+<div>
+    <?php 
+        $letra = chr(67);
+        echo "A letra de código 67 é $letra";    
+    ?>
+</div>
+</body>
+</html>
