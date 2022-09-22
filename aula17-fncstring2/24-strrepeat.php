1de73c80dccc528d5293a01c1cb0ed54a6818e2e
Leonardo Fernandes Jeronimo <leonardo.lfj133@gmail.com>
2022-09-22 13:30:21 -0300

    str repeat

---
+++ aula17-fncstring2/24-strrepeat.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+<div>
+    <?php 
+        //Repete a string
+        $txt = str_repeat("Php",5);
+        print("O texto gerado foi $txt");
+    ?>
+</div>
+</body>
+</html>
