c9e6a39b4d61ac2f314da9dc9aea3cec739846c3
Leonardo Fernandes Jeronimo <leonardo.lfj133@gmail.com>
2022-09-22 13:27:32 -0300

    strpad

---
+++ aula17-fncstring2/23-strpad.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+<div>
+    <?php 
+        //Encontra a posição da string
+        $nome = "Leonardo";
+        $novo = str_pad($nome, 50, " ",STR_PAD_RIGHT);
+        print("Meu aluno $novo é muito dedicado!");
+    ?>
+</div>
+</body>
+</html>
