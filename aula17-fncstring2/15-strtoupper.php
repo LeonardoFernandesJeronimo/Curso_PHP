3bc277f25e1d1dff71649aa5819e9cb132af2fbc
Leonardo Fernandes Jeronimo <leonardo.lfj133@gmail.com>
2022-09-22 12:53:42 -0300

    str to upper

---
+++ aula17-fncstring2/15-strtoupper.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP</title>
+</head>
+<body>
+<div>
+    <?php 
+        //Deixa tudo em letra maiuscula
+        $nome = "Leonardo Fernandes";
+        $nome2 = strtoupper($nome);
+        echo "Seu nome é $nome2";
+    ?>
+</div>
+</body>
+</html>
